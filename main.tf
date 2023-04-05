1b62848d16d21ddffb3329c5bdd13b96be446e4b
raavimanikanta <raavimani123@gmail.com>
2023-04-05 15:49:52 +0530

    added changes

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "newrelic_alert_policy" "foo" {
   # count=length(var.alert_policy_name)
   # name ="${lookup(var.alert_policy_name[count.index],"name")}"
   name = var.alert_policy_name
-  incident_preference = "PER_POLICY" # PER_POLICY is default
+  // incident_preference = "PER_POLICY" # PER_POLICY is default
 }
 
