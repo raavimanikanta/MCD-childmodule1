09efb217c8b1ba9c7eb82b8706b23d9ecc1f1859
raavimanikanta <raavimani123@gmail.com>
2023-04-05 20:41:08 +0530

    added changes in child module

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 resource "newrelic_alert_policy" "foo" {
-  # count=length(var.alert_policy_name)
-  # name ="${lookup(var.alert_policy_name[count.index],"name")}"
-  name = var.alert_policy_name
+   name = var.alert_policy_name
   // incident_preference = "PER_POLICY" # PER_POLICY is default
 }
 
